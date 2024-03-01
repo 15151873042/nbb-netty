f40233fe48269bf046b6d3d0430a7764c9db13ab
胡鹏 <15151873042@163.com>
2024-03-01 17:19:35 +0800

    确认netty-server启动端口是否绑定成功

--- nbb-springboot-netty/src/main/java/com/nbb/netty/springboot/NettyServerApplicationRunner.java
+++ nbb-springboot-netty/src/main/java/com/nbb/netty/springboot/NettyServerApplicationRunner.java
@@ -1,17 +1,18 @@
-package com.nbb.netty.springboot.framework.netty;
+package com.nbb.netty.springboot;
 
+import com.nbb.netty.springboot.framework.netty.NettyServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NettyStartListener implements ApplicationRunner {
+public class NettyServerApplicationRunner implements ApplicationRunner {
 
     @Autowired
     private NettyServer nettyServer;
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         nettyServer.start();
     }
 }
--- nbb-springboot-netty/src/main/java/com/nbb/netty/springboot/framework/netty/NettyServer.java
+++ nbb-springboot-netty/src/main/java/com/nbb/netty/springboot/framework/netty/NettyServer.java
@@ -1,13 +1,18 @@
 package com.nbb.netty.springboot.framework.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
 
 @Slf4j
 @Component
@@ -17,33 +22,69 @@ public class NettyServer {
     @Autowired
     private NettyProperties nettyProperties;
 
-    public ServerBootstrap serverBootstrap;
+    /** netty-server启动端口绑定成功的标识 */
+    private AtomicBoolean nettyPortBind = new AtomicBoolean(false);
+
+    /** 当前springBoot项目启动主线程 */
+    private Thread springBootStartThread;
 
 
     /**
      * 启动netty服务器
      */
     public void start() {
+        this.springBootStartThread = Thread.currentThread();
+
         NioEventLoopGroup bossGroup = new NioEventLoopGroup(nettyProperties.getBoosThread());
         NioEventLoopGroup workerGroup = new NioEventLoopGroup(nettyProperties.getWorkerThread());
 
         try {
-            this.serverBootstrap = new ServerBootstrap();
-            this.serverBootstrap.group(bossGroup, workerGroup)
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                        @Override
-                        protected void initChannel(NioSocketChannel channel) throws Exception {
-                            log.info("====有新的客户端【{}】连接进来了，当前通道id为【{}】====", channel.remoteAddress(), channel.id());
-                        }
-                    });
+                    .childHandler(new NettyServerChannelInitializer());
 
-            this.serverBootstrap.bind(nettyProperties.getPort());
-            log.info("Netty在{}端口启动", nettyProperties.getPort());
+            // netty-server启动绑定对应端口
+            ChannelFuture portBindFuture = serverBootstrap.bind(nettyProperties.getPort());
+            // 添加netty-server端口绑定任务处理完成后的回调
+            portBindFuture.addListener(this::portBindComplete);
+
+            // 主线程等待netty-server端口绑定任务处理结果
+            LockSupport.park();
+
+            if (Boolean.FALSE.equals(nettyPortBind.get())) {
+                log.error("Netty-server在{}端口启动失败", nettyProperties.getPort());
+                throw new RuntimeException("Netty-server启动失败");
+            }
+
+            log.info("Netty-server在{}端口启动成功", nettyProperties.getPort());
+
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
     }
+
+
+    /**
+     * netty-server启动绑定端口后的回调
+     * @param future
+     */
+    private void portBindComplete(Future future) {
+        boolean portBindSuccess = future.isSuccess();
+        nettyPortBind.set(portBindSuccess);
+
+        LockSupport.unpark(springBootStartThread);
+
+        springBootStartThread = null;
+    }
+
+    private static class NettyServerChannelInitializer extends ChannelInitializer<NioSocketChannel> {
+        @Override
+        protected void initChannel(NioSocketChannel ch) {
+            log.info("====有新的客户端【{}】连接进来了，当前通道id为【{}】====", ch.remoteAddress(), ch.id());
+        }
+    }
+
 
 }
